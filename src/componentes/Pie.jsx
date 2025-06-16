1d4b40a46cd46d1247e0504d9d844e15198cae3a
AleyssaSamantha <aleyssacubam@gmail.com>
2025-06-16 02:03:24 -0400

    Renombré Footer a Pie y actualicé App.jsx

--- src/componentes/Pie.jsx
+++ src/componentes/Pie.jsx
@@ -1,4 +1,4 @@
-export function Pie() {
+export default function Pie() {
   return (
     <footer className="bg-gray-800 text-white py-6 mt-10 w-full flex flex-col items-center"
     style={{
@@ -9,9 +9,7 @@ export function Pie() {
       }}
     >
       <p className="text-lg font-semibold">© 2025 RAÍCES DE BOLIVIA </p>
-      <p className="text-sm">Todos los derechos reservados por Aleyssa Samantha Cuba Mamani</p>
+      <p className="text-sm">Todos los derechos reservados por Aleyssa Samantha Cuba</p>
     </footer>
   );
 }
-
-export default Pie;
